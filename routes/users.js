0c0e1852cc920704c437578de7e5e96ded1b9bd6
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-23 13:00:47 -0500

    add login and  registeroutes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userCtrl = require("../controllers/users");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userCtrl.index);
 
+/* GET login  page. */
+router.get("/login", userCtrl.login);
+/* GET register  page. */
+router.get("/register", userCtrl.register);
+
+/* GET register  page. */
+router.post("/register", function (req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
 module.exports = router;
