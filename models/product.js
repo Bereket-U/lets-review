47af63a93c46cebce47345c92e247e8da25e7f25
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-24 15:17:05 -0500

    add product model

---
+++ models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema(
+  {
+    content: String,
+    rating: { type: Number, min: 1, max: 5, default: 5 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: String, required: true },
+    discription: { type: String, required: true },
+    category: { type: String, required: true },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
