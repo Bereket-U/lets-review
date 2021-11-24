dd56fd00c957b3a5f4d4c1bc7d552fe862f33b17
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-24 18:13:25 -0500

    add product create function

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,37 @@
 const Product = require("../models/product");
 
+// All products
 function allProduct(req, res, next) {
-  res.render("users/products/index");
+  res.render("users/products/index", {
+    title: "Add Products",
+    message: req.flash("message"),
+  });
+}
+// Add product
+function addProduct(req, res, next) {
+  res.render("users/products/add", {
+    title: "Add Products",
+    message: req.flash("message"),
+  });
 }
 
-function addProduct(req, res, next) {
-  res.render("users/products/add");
+function create(req, res, next) {
+  console.log(req.body);
+  const product = new Product(req.body);
+  product.save(function (err) {
+    if (err) {
+      req.flash("message", "Please try again!");
+      return res.redirect("/users/products/add");
+    }
+
+    req.flash("message", "Congratulations, your product has been added");
+    res.redirect("/users/products/");
+    console.log("added");
+  });
 }
 
 module.exports = {
   allProduct,
   addProduct,
+  create,
 };
