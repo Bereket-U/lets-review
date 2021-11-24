294d43fb878c72fcd13f7a18f0509bcc643ae775
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-23 21:55:26 -0500

    add login and logout functionality

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const userCtrl = require("../controllers/users");
 
-/* GET users listing. */
+/* GET users home listing. */
 router.get("/", userCtrl.index);
 
 /* GET login  page. */
@@ -10,6 +10,11 @@ router.get("/login", userCtrl.login);
 /* GET register  page. */
 router.get("/register", userCtrl.register);
 
-/* GET register  page. */
+/* POST register  page. */
 router.post("/register", userCtrl.newUser);
+
+/* POST login  page. */
+router.post("/login", userCtrl.loginPassport);
+router.get("/logout", userCtrl.logout);
+
 module.exports = router;
