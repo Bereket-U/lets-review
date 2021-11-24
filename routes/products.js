c803bd2a14f84dbdbb69d585c11032b3b6fc5741
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-24 18:12:05 -0500

    add product create router

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,13 @@ var router = express.Router();
 const productCtrl = require("../controllers/products");
 const { ensureAuthenticated } = require("../config/auth");
 
+/* GET Product Index  page. */
 router.get("/", ensureAuthenticated, productCtrl.allProduct);
+
+/* GET Add Product  page. */
 router.get("/add", ensureAuthenticated, productCtrl.addProduct);
+
+/* POST create product  page. */
+router.post("/create", productCtrl.create);
 
 module.exports = router;
