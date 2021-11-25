34d78b839756f1043bf2fb45a4f4776ca4370f42
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-25 18:31:51 -0500

    add review bug fix

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,11 +2,14 @@ const Product = require("../models/product");
 
 // Create review
 function create(req, res, next) {
+  //   res.send("hello" + req.params.userId);
+
   Product.findById(req.params.id, function (err, product) {
-    console.log(product.reviews);
+    const userId = req.params.userId;
+    // console.log(product.reviews);
     product.reviews.push(req.body);
     product.save(function (err) {
-      res.render("users/products/show", { product });
+      res.render(`users/products/show`, { product, userId });
     });
   });
 }
